192c6831400749f940c5ffd5e5a239ec7f066247
Cico <feciconini@gmail.com>
2024-02-08 14:11:50 +0000

    added new planet module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/ui/layout/layout.component'
+import { planetRoutes } from './planets/planet-shell/planet.routes'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "planets",
+        component: LayoutComponent,
+        children: planetRoutes
+    }
+];

6833313c2a7b06840d47d1cf9d65c1cac421c4e5
Cico <feciconini@gmail.com>
2024-02-16 11:47:42 +0000

    style(film-detail): fixed film-detail responsive and added movie background videos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http'
 
+// go to top when route changes config
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+const inMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, inMemoryScrollingFeature),
     provideHttpClient(),
     provideAnimationsAsync()
   ]
